8724de4613e10a9f712478d523c4c722b3afb4eb
Tianhu Yang <tianhuyang@dynamic2-250-031.usc.edu>
2012-05-28 22:46:53 -0700

    Initial Commit

---
+++ keybox/AppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.h
+//  keybox
+//
+//  Created by Tianhu Yang on 5/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@property (strong, nonatomic) UISplitViewController *splitViewController;
+
+@end
---
+++ keyboxTests/keyboxTests.h
@@ -0,0 +1,13 @@
+//
+//  keyboxTests.h
+//  keyboxTests
+//
+//  Created by Tianhu Yang on 5/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface keyboxTests : SenTestCase
+
+@end
